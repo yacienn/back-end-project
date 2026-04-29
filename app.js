de9cc682371036f88f723e9802f9cbd0ae241da2
yacienn <yacine.djabali@univ-alger.dz>
2026-04-29 18:36:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
-const port = 3000 ;
+const port = 3000;
+app.use(express.json());
 const users = []
-app.get('/users' , (req , res)=>{
-     res.status(201).send(users);
-});
+
+app.post('/sign_up' , (req , res)=>{
+   const { email, password } = req.body;
+   const userExist = users.find(u => u.email === email);
+    if (userExist) {
+        return res.status(409).json({
+            message: "User already exists"
+        });
+    }
+    const newUser = {email , password};
+    users.push(newUser);
+    res.status(201).json({
+        message : "user create successfuly" ,
+        user : newUser
+    });
+})
+
+app.get('/regester' , (req , res)=>{
+   res.send(users)
+})
+
 
 app.listen(port , ()=>{
-    console.log(`i listen on port ${port}`);
+    console.log(`lisetning in port ${port}`);
 })
