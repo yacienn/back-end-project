9d9fbd83c8a3ee3fc211f96354affb90ca44da79
yacienn <yacine.djabali@univ-alger.dz>
2026-04-30 15:33:48 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "nithce",
+  database: "nitche",
   password: "yacine",
   port: 5432,
 });
