fdea83676917cd5a187ab81571b3e916d434bec4
yacienn <yacine.djabali@univ-alger.dz>
2026-05-02 21:35:37 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 import pkg from "pg";
+import dotenv from "dotenv";
 const { Pool } = pkg;
+dotenv.config();
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "nitche",
-  password: "yacine",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
