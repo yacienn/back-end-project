f8dc3ec9b8e99fa2d4600974fcbb4ffb9a7a02fa
yacienn <yacine.djabali@univ-alger.dz>
2026-05-03 12:59:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from '../backend/routers/auth_router.js'
 
 const app = express();
 const port = 3000;
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.json());
 
 // routes
-app.use('/api/auth', authRoutes);
+app.use('/', authRoutes);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
