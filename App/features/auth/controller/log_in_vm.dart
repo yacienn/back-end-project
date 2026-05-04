28d70017e59b8a48711d10d15bba863e1ff07259
yacienn <yacine.djabali@univ-alger.dz>
2026-05-04 16:18:31 +0100

    Add log in method with API integration
    Implement log in functionality with API call and error handling.

--- App/features/auth/controller/log_in_vm.dart
+++ App/features/auth/controller/log_in_vm.dart
@@ -1,14 +1,45 @@
+import 'package:app/core/api/api_service.dart';
+import 'package:app/features/auth/model/auth_model.dart';
 import 'package:flutter/material.dart';
 
-class LogInVm extends ChangeNotifier{
-   final emailController = TextEditingController();
-   final passwordController = TextEditingController();
+class LogInVm extends ChangeNotifier {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
-   void logIn(){
+  final ApiService apiService = ApiService();
+
+  String? errorMessage;
+
+  Future<void> logIn() async { 
+    errorMessage = null;
     notifyListeners();
-   } 
-   
-    @override
+
+  
+    final log = AuthModel(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+
+    final body = log.toJson();
+
+    // 4. call backend
+    final response = await apiService.post("log_in", body);
+
+    // 5. handle response
+    if (response["success"]) {
+      print("LOGIN SUCCESS");
+      print(response["data"]);
+    } else {
+      errorMessage = response["data"]["message"];
+      print("LOGIN FAILED: $errorMessage");
+    }
+
+    // 6. stop loading
+
+    notifyListeners();
+  }
+
+  @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
