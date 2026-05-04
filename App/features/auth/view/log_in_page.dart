c4167a1b854b6f57254c50e03927087a520ba182
yacienn <yacine.djabali@univ-alger.dz>
2026-05-04 16:20:22 +0100

    Update log_in_page.dart

--- App/features/auth/view/log_in_page.dart
+++ App/features/auth/view/log_in_page.dart
@@ -1,4 +1,4 @@
-import 'package:app/controller/log_in_vm.dart';
+import 'package:app/features/auth/controller/log_in_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,37 +8,46 @@ class LogInPage extends StatefulWidget {
   @override
   State<LogInPage> createState() => _LogInPageState();
 }
+
 class _LogInPageState extends State<LogInPage> {
-  
-    @override
+  @override
   Widget build(BuildContext context) {
     final vm = context.watch<LogInVm>();
     return Scaffold(
-      appBar: AppBar(title: Text("Log_in"), centerTitle: true,),
-      body: Padding(padding: EdgeInsets.all(25), 
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             TextField(
-              controller: vm.emailController,
-              decoration: InputDecoration(
-                label: Text("email"),
-                border: OutlineInputBorder(),
+      appBar: AppBar(title: Text("Log_in"), centerTitle: true),
+      body: Padding(
+        padding: EdgeInsets.all(25),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: vm.emailController,
+                decoration: InputDecoration(
+                  label: Text("email"),
+                  border: OutlineInputBorder(),
+                ),
               ),
-             ), SizedBox(height: 20,),
-             TextField(
-              controller: vm.passwordController,
-              decoration: InputDecoration(
-                label: Text("password"),
-                border: OutlineInputBorder(),
+              SizedBox(height: 20),
+              TextField(
+                controller: vm.passwordController,
+                decoration: InputDecoration(
+                  label: Text("password"),
+                  border: OutlineInputBorder(),
+                ),
               ),
-             ), SizedBox(height: 20,),
-             ElevatedButton(onPressed: ()=> context.read<LogInVm>().logIn(), child: Text("log_in"))
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<LogInVm>().logIn();
+                  Navigator.pushReplacementNamed(context, '/HomePage');
+                },
+                child: Text("log_in"),
+              ),
+            ],
+          ),
         ),
-      ),),
+      ),
     );
   }
-  
 }
