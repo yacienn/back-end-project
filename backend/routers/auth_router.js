fecc9766705247410b4af6e813e6a986a7f37dfc
yacienn <yacine.djabali@univ-alger.dz>
2026-05-06 17:20:56 +0200

    Add authentication middleware to getUsers route

--- backend/routers/auth_router.js
+++ backend/routers/auth_router.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {signUp , logIn , getUsers} from '../controllers/auth_controller.js'
+import { signUp, logIn, getUsers } from '../controllers/auth_controller.js';
+import { authenticateToken } from '../middleware/auth_middleware.js';
 
 const router = express.Router();
 
-router.post('/sing_up' , signUp);
-router.post('/log_in' , logIn);
-router.get('/regester' , getUsers);
+router.post('/sing_up', signUp);
+router.post('/log_in', logIn);
+router.get('/regester', authenticateToken, getUsers);  
 
-export default router ;
+export default router;
