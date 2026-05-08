bac158ed7538a7666fffe64ab447bf6008547bfc
yacienn <yacine.djabali@univ-alger.dz>
2026-05-08 17:59:13 +0200

    Add CORS support to the server
    Added CORS middleware to enable cross-origin requests.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import authRoutes from '../backend/routers/auth_router.js'
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
-// routes
+
 app.use('/', authRoutes);
 
 app.listen(port, () => {
