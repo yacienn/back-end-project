b8d1190c2c52d67eb83ceeedda4fc2f27a776c63
yacienn <yacine.djabali@univ-alger.dz>
2026-05-14 17:53:52 +0200

    Update auth_router.js

--- backend/routers/auth_router.js
+++ backend/routers/auth_router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { signUp, logIn, getUsers } from '../controllers/auth_controller.js';
-import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/sing_up', signUp);
 router.post('/log_in', logIn);
-router.get('/users', authenticateToken, getUsers);
+router.get('/users', getUsers);
+
+
 
 export default router;
